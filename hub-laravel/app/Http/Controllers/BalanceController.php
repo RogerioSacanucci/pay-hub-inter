4169fd40f6a13900f03e7952ac6c9c4f85ec5356
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-03-29 09:15:53 -0300

    feat: add GET /balance endpoint for authenticated users (#FRA-61)

---
+++ hub-laravel/app/Http/Controllers/BalanceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BalanceController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        $balance = $request->user()->balance;
+
+        return response()->json([
+            'balance_pending' => $balance?->balance_pending ?? '0.000000',
+            'balance_released' => $balance?->balance_released ?? '0.000000',
+            'currency' => $balance?->currency ?? 'USD',
+        ]);
+    }
+}
