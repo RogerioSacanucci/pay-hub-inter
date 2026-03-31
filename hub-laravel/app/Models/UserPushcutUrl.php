ea0fc1e61bf10780cbefbb132262c56bb33abcc2
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-03-31 09:27:06 -0300

    fix: remove user_id from UserPushcutUrl fillable

--- hub-laravel/app/Models/UserPushcutUrl.php
+++ hub-laravel/app/Models/UserPushcutUrl.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['user_id', 'url', 'notify', 'label'])]
+#[Fillable(['url', 'notify', 'label'])]
 class UserPushcutUrl extends Model
 {
     /** @use HasFactory<UserPushcutUrlFactory> */
