817c7449508bd31af310e635fe6550042541fc45
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-04-09 16:32:42 -0300

    add caramelo sec

--- hub-laravel/routes/web.php
+++ hub-laravel/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/caramelosectoken', function () {
+    return response()->json(['caramelosec-token' => 'd54028ad-53d1-4d05-9606-b6b6e82e5a56']);
+});
