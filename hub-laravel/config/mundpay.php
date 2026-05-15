5a186dbaedbde2818329654d728cb83d6ae8825a
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-05-14 23:31:21 -0300

    adjust email

--- hub-laravel/config/mundpay.php
+++ hub-laravel/config/mundpay.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'user_email' => env('MUNDPAY_USER_EMAIL'),
+    'user_email' => env('MUNDPAY_USER_EMAIL', 'srretry@fractal.com'),
     'reserve_rate' => (float) env('MUNDPAY_RESERVE_RATE', 0.15),
     'release_delay_days' => (int) env('MUNDPAY_RELEASE_DELAY_DAYS', 3),
     'brl_usd_rate' => (float) env('MUNDPAY_BRL_USD_RATE', 5.0),
