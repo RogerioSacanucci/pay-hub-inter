77052ec8c6b56c32fb6a36eba273e0413d4134c6
RogerioSacanucci <rogeriosacanucci02@gmail.com>
2026-05-14 22:36:52 -0300

    feat: add mundpay config

---
+++ hub-laravel/config/mundpay.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'user_email' => env('MUNDPAY_USER_EMAIL'),
+    'reserve_rate' => (float) env('MUNDPAY_RESERVE_RATE', 0.15),
+    'release_delay_days' => (int) env('MUNDPAY_RELEASE_DELAY_DAYS', 3),
+    'brl_usd_rate' => (float) env('MUNDPAY_BRL_USD_RATE', 5.0),
+];
